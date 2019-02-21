ccf40d15556b4edbc98ca6545c2cb7e9f4d353f2
Full-Stack developer <83703580+noveltyz@users.noreply.github.com>
2019-02-22 00:01:00 +0900

    Update

--- helper.js
+++ helper.js
@@ -5184,3 +5184,5 @@ Contribution: 2019-02-21 00:01
 
 Contribution: 2019-02-22 00:00
 
+Contribution: 2019-02-22 00:01
+
---
+++ server/.eslintrc.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+module.exports = {
+  root: true,
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: "module",
+    allowImportExportEverywhere: false,
+  },
+  env: {
+    node: true,
+    jest: true
+  },
+  rules: {
+    // override defaults
+    eqeqeq: [2, 'smart'],
+    'max-len': [2, { 'code': 120, 'tabWidth': 2, 'ignoreUrls': true }],
+    'newline-per-chained-call': [2, { ignoreChainWithDepth: 4 }],
+    'no-cond-assign': [2, 'except-parens'],
+    'no-multi-spaces': [2, { exceptions: {
+      ImportDeclaration: true,
+      Property: true,
+      VariableDeclarator: true,
+    }}],
+    'prefer-destructuring': [2, {
+      array: false,
+      object: true
+    }, {
+      enforceForRenamedProperties: false
+    }],
+    'space-before-function-paren': [2, 'never'],
+
+    // disable defaults
+    'arrow-parens': 0,
+    'brace-style': 0,
+    'consistent-return': 0,
+    'func-names': 0,
+    'implicit-arrow-linebreak': 0,
+    'no-confusing-arrow': 0,
+    'no-console': 0,
+    'no-nested-ternary': 0,
+    'no-param-reassign': 0,
+    'no-plusplus': 0,
+    'no-shadow': 0,
+    'no-underscore-dangle': 0,
+    'object-curly-newline': 0,
+    'padded-blocks': 0,
+  },
+};
